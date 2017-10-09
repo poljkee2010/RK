77c2c806aab960f643e9bd3c5a56a10c00af62f8
poljkee2010 <31631937+poljkee2010@users.noreply.github.com>
2017-10-09 12:44:27 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+	int data;
+	Node * next;
+};
+
+class List
+{
+	Node *Head;
+public:
+	List() 
+	{ 
+		Head = NULL; 
+	}
+	~List()
+	{
+		while (Head != NULL)
+		{
+			Node *temp = Head->next;
+			delete Head;
+			Head = temp;
+		}
+	}
+	void Add(int x)
+	{
+		Node *temp = new Node;
+		temp->data = x;
+		temp->next = Head;
+		Head = temp;
+	}
+	void Show()
+	{
+		Node *temp = Head;
+		while (temp != NULL)
+		{
+			cout << temp->data << "";
+			temp = temp->next;
+		}
+	}
+	Node * reverse(Node * head)
+	{
+		Node *rev = NULL;
+		for (Node *node = head, *next_nod; node != NULL; node = next_nod)
+		{
+			next_nod = node->next;
+			node->next = rev;
+			rev = node;
+		}
+		return rev;
+	}
+};
